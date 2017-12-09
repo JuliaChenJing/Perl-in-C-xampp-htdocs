ae78ea784ae9adc43043a667393abf3d42088553
Julia Chen <juliachenjing@gmail.com>
2017-12-09 09:38:02 -0500

    updated url

--- P/insertData.pl
+++ P/insertData.pl
@@ -1,6 +1,7 @@
 #!C:\xampp\perl\bin\perl.exe
 
-print "Content-type : text/html\n\n";
+       
+
 use DBI;
 use strict;
 use CGI;
@@ -10,6 +11,7 @@ my $database="atnt.db";
 my $dsn="DBI:$driver:dbname=$database";
 my $userid="";
 my $password="";
+
 
 #DATABASE CONNECTION
 my $db_handle = DBI -> connect($dsn,$userid,$password,{RaiseError=>1}) or die $DBI::errstr;
@@ -21,15 +23,26 @@ my $txtDate = $cgi->param('txtDate');
 my $txtTime = $cgi->param('txtTime');
 my $txtDescription = $cgi->param('txtDescription');
 
+
 #INSERTING TO RECORD TABLE
 my $sth= $db_handle -> prepare("insert into record(datetime, description) values (?,?);");
 
+  
 #EXECUTE INSERT STATEMENT COMBINING DATE AND TIME
 $sth->execute($txtDate." ".$txtTime, $txtDescription) or die $DBI::errstr;
 
 $db_handle->disconnect();
 
+
 #AUTO RE-DIRECT AFTER DATA ENTRY
-my $url="http://localhost/ATnT/index.html";
+my $url="http://localhost/perl/p/index.html";
+
+
+
 my $t=0; # time until redirect activates
+
+
 print "<META HTTP-EQUIV=refresh CONTENT=\"$t;URL=$url\">\n";
+#print "Content-type: text/html\n\n";
+#print "Hello world." 
+
